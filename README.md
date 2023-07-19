70ed3f53167acb2d70a8db0b71b655e1a1478141
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2023-07-19 10:51:09 +0200

    feat: plugin structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Strack Integrations
+
+Shopware 6 Plugins for Strack Integrations with extern services
+
+
+## Live price requests
---
+++ src/StrackIntegrations.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StrackIntegrations;
+
+use Shopware\Core\Framework\Plugin;
+
+class StrackIntegrations extends Plugin
+{
+}
