3282cf1abb8a03748ff29d4cda4737813f979602
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2023-07-20 08:44:32 +0200

    feat: Added PriceClient

--- src/Client/AbstractClient.php
+++ src/Client/AbstractClient.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace StrackIntegrations\Client;
 
 use GuzzleHttp\Client;
+use SimpleXMLElement;
 use StrackIntegrations\Config\ApiConfig;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
@@ -14,34 +15,40 @@ readonly abstract class AbstractClient
     const CACHE_TOKEN_KEY = 'strack_integrations.access_token';
     const TEMPLATE_PLACEHOLDER = '%%%template_json%%%';
 
+    private Client $client;
+
     public function __construct(
         protected ApiConfig $apiConfig,
-        private CacheInterface $cache,
-        private Client $client
-    ) { }
+        private CacheInterface $cache
+    ) {
+        $this->client = new Client();
+    }
 
-    public function post(string $endpoint, string $soapAction, array $jsonParams): array
+    protected function post(string $endpoint, string $soapAction, string $envelope, array $jsonParams): SimpleXMLElement
     {
         $response = $this->client->post($this->apiConfig->getApiDomain() . $endpoint, [
            'headers' => [
                'Authorization' => 'Bearer ' . $this->getAccessToken(),
                'SOAPAction' => $soapAction,
-               'Context-Type' => 'text/xml'
+               'Content-Type' => 'text/xml',
+               'Accept' => 'text/xml'
            ],
-            'body' => $this->getRequestBody($jsonParams)
+            'body' => $this->getRequestBody($envelope, $jsonParams)
         ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return new SimpleXMLElement($response->getBody()->getContents());
     }
 
     private function getAccessToken(): string
     {
         return $this->cache->get(self::CACHE_TOKEN_KEY, function (ItemInterface $item) {
             $response = $this->client->post($this->apiConfig->getAccessTokenUri(), [
-                'grant_type' => 'client_credentials',
-                'scope' => $this->apiConfig->getScope(),
-                'client_secret' => $this->apiConfig->getClientSecret(),
-                'client_id' => $this->apiConfig->getClientId()
+                'form_params' => [
+                    'grant_type' => 'client_credentials',
+                    'scope' => $this->apiConfig->getScope(),
+                    'client_secret' => $this->apiConfig->getClientSecret(),
+                    'client_id' => $this->apiConfig->getClientId()
+                ]
             ]);
 
             $arrayResponse = json_decode($response->getBody()->getContents(), true);
@@ -52,22 +59,8 @@ readonly abstract class AbstractClient
         });
     }
 
-    private function getRequestBody(array $params): string
+    private function getRequestBody(string $envelope, array $params): string
     {
-        return str_replace(self::TEMPLATE_PLACEHOLDER, json_encode($params), $this->getEnvelopeTemplate());
-    }
-
-    private function getEnvelopeTemplate(): string
-    {
-        return <<<XML
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:lic="urn:microsoft-dynamics-schemas/codeunit/SWWebServices">
-    <soapenv:Header></soapenv:Header>
-    <soapenv:Body>
-        <lic:GetSalesPrice>
-            <lic:parameter>%%%template_json%%%</lic:parameter>
-        </lic:GetSalesPrice>
-    </soapenv:Body>
-</soapenv:Envelope>
-XML;
+        return str_replace(self::TEMPLATE_PLACEHOLDER, json_encode($params), $envelope);
     }
 }
--- src/Client/PriceClient.php
+++ src/Client/PriceClient.php
@@ -4,30 +4,60 @@ declare(strict_types=1);
 
 namespace StrackIntegrations\Client;
 
+use StrackIntegrations\Exception\NoReturnValueException;
 use StrackIntegrations\Struct\SalesPrice;
 
 readonly class PriceClient extends AbstractClient
 {
     public function getSalesPrice(string $debtorNumber, string $productNumber, int $quantity): ?SalesPrice
     {
-        $response = $this->post($this->apiConfig->getPriceEndpoint(), $this->apiConfig->getPriceSoapAction(), [
-            'customerId' => $debtorNumber,
-            'itemNo' => $productNumber,
-            'quantity' => $quantity
-        ]);
+        $response = $this->post(
+            $this->apiConfig->getPriceEndpoint(),
+            $this->apiConfig->getPriceSoapAction(),
+            $this->getSalesPriceEnvelope(),
+            [
+                'customerId' => $debtorNumber,
+                'itemNo' => $productNumber,
+                'quantity' => $quantity
+            ]
+        );
 
-        $this->validateResponse($response);
+        $namespaces = $response->getNamespaces(true);
+        $swWebServices = $response->children($namespaces['Soap'])->Body->children($namespaces['']);
+
+        if (!isset($swWebServices->GetSalesPrice_Result->return_value)) {
+            throw new NoReturnValueException($response->asXML());
+        }
+
+        $returnValue = (string)$swWebServices->GetSalesPrice_Result->return_value;
+        $jsonResponse = json_decode($returnValue, true, 512, JSON_THROW_ON_ERROR);
+
+        $this->validateResponse($jsonResponse);
 
         return (new SalesPrice())
-            ->setProductNumber($response['No.'])
-            ->setDebtorNumber($response['Sell-to Customer No.'])
-            ->setUnitPrice($response['Unit Price'])
-            ->setQuantity($response['Quantity'])
-            ->setPercentageLineDiscount($response['Line Discount %'])
-            ->setTotalPrice($response['LineAmount'])
-            ->setTotalPriceWithVat($response['AmountIncluding VAT'])
-            ->setIsBrutto($response['Prices Including VAT'])
-            ->setCurrencyIso($response['Currency Code']);
+            ->setProductNumber($jsonResponse['No.'])
+            ->setDebtorNumber($jsonResponse['Sell-to Customer No.'])
+            ->setUnitPrice($jsonResponse['Unit Price'])
+            ->setQuantity($jsonResponse['Quantity'])
+            ->setPercentageLineDiscount($jsonResponse['Line Discount %'])
+            ->setTotalPrice($jsonResponse['Line Amount'])
+            ->setTotalPriceWithVat($jsonResponse['Amount Including VAT'])
+            ->setIsBrutto($jsonResponse['Prices Including VAT'])
+            ->setCurrencyIso($jsonResponse['Currency Code']);
+    }
+
+    private function getSalesPriceEnvelope(): string
+    {
+        return <<<XML
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:lic="urn:microsoft-dynamics-schemas/codeunit/SWWebServices">
+    <soapenv:Header></soapenv:Header>
+    <soapenv:Body>
+        <lic:GetSalesPrice>
+            <lic:parameter>%%%template_json%%%</lic:parameter>
+        </lic:GetSalesPrice>
+    </soapenv:Body>
+</soapenv:Envelope>
+XML;
     }
 
     private function validateResponse(array $response): void
