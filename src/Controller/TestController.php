a02270b348571a3f67175cc4385729ef24749894
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2023-07-21 11:57:58 +0200

    feat: added test controller

---
+++ src/Controller/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StrackIntegrations\Controller;
+
+use GuzzleHttp\Exception\BadResponseException;
+use Shopware\Storefront\Controller\StorefrontController;
+use StrackIntegrations\Client\PriceClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends StorefrontController
+{
+    public function __construct(
+        protected PriceClient $priceClient
+    ) {
+    }
+
+    #[Route('/StrackIntegrations/test', name: 'frontend.StrackIntegrations.test', defaults: ['_routeScope' => ['storefront'], 'csrf_protected' => false], methods: ['GET'])]
+    public function generateProductDataFeed(): Response
+    {
+        try {
+            $test = $this->priceClient->getSalesPrice('10001868', '173297', 3);
+            var_dump($test);
+            exit;
+        } catch(BadResponseException $exception) {
+            var_dump($exception->getResponse()->getBody()->getContents());
+            exit;
+        }
+    }
+}
