7a9fceee9eb154c8bafbee8bdd9edb44a56c0315
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2023-11-24 14:18:14 +0100

    feat: monolog logger for 6.5 compatibility

--- src/StrackIntegrations.php
+++ src/StrackIntegrations.php
@@ -9,9 +9,35 @@ use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 use StrackIntegrations\Installer\CustomerCustomFieldsInstaller;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class StrackIntegrations extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $locator = new FileLocator('Resources/config');
+
+        $resolver = new LoaderResolver([
+            new YamlFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = \rtrim($this->getPath(), '/') . '/Resources/config';
+
+        $configLoader->load($confDir . '/{packages}/*.yaml', 'glob');
+    }
+
     public function install(InstallContext $installContext): void
     {
 //        (new CustomerCustomFieldsInstaller($this->container))->installCustomFieldSet($installContext->getContext());
