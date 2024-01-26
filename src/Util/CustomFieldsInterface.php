904988c256b5f8ec63ae3f0009d651f3a2b7081b
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2024-01-26 09:58:54 +0100

    feat: added new order custom fields

--- src/Util/CustomFieldsInterface.php
+++ src/Util/CustomFieldsInterface.php
@@ -6,9 +6,11 @@ namespace StrackIntegrations\Util;
 
 interface CustomFieldsInterface
 {
+    // Customer custom fields
     public const CUSTOMER_CUSTOM_FIELD_SET = 'strack_integrations_customer';
     public const CUSTOMER_MINIMUM_ORDER_VALUE = 'strack_customer_minimum_order_value';
 
+    // Order custom fields
     public const ORDER_CUSTOM_FIELD_SET = 'strack_integrations_order';
     public const ORDER_IS_OFFER = 'strack_order_has_price_error';
     public const ORDER_REQUESTED_DELIVERY_DATE = 'strack_order_requested_delivery_date';
@@ -16,4 +18,8 @@ interface CustomFieldsInterface
     public const ORDER_OWN_ORDER_NUMBER = 'strack_order_own_order_number';
     public const ORDER_OFFER_NUMBER = 'strack_order_offer_number';
     public const ORDER_COMMENT = 'strack_order_comment';
+
+    // Cart/Order position payload keys
+    public const ORDER_POSITION_OWN_PART_NUMBER = 'strack_order_position_own_part_number';
+    public const ORDER_POSITION_COMMENT = 'strack_order_position_comment';
 }
