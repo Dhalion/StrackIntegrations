d7cfd386859b242fef2f7eb84ca422d5142e9b62
Christopher Falkner <c.falkner@agiqon.de>
2024-02-02 08:57:52 +0100

    feat: add minimum order value warning
    SN-109

---
+++ src/Subscriber/CartLoadedSubscriber.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace StrackIntegrations\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Storefront\Page\Checkout\Cart\CheckoutCartPageLoadedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use StrackIntegrations\Util\CustomFieldsInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Shopware\Storefront\Page\Checkout\Offcanvas\OffcanvasCartPageLoadedEvent;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Checkout\Cart\Cart;
+
+readonly class CartLoadedSubscriber implements EventSubscriberInterface {
+    public function __construct(
+        private RequestStack $requestStack,
+        private TranslatorInterface $translator,
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            CheckoutCartPageLoadedEvent::class => 'handleMinimumOrderValue',
+            OffcanvasCartPageLoadedEvent::class => 'handleMinimumOrderValue'
+        ];
+    }
+
+    public function handleMinimumOrderValue(CheckoutCartPageLoadedEvent | OffcanvasCartPageLoadedEvent $event): void {
+        $customer = $event->getSalesChannelContext()->getCustomer();
+        if (!$customer) {
+            return;
+        }
+
+        $minimumOrderValue = $customer->getCustomFields()[
+            CustomFieldsInterface::CUSTOMER_MINIMUM_ORDER_VALUE
+        ] ?? null;
+        if (!$minimumOrderValue) {
+            return;
+        }
+
+        $cart = $event->getPage()->getCart();
+        $cartAmount = $cart->getPrice()->getTotalPrice();
+
+        if ($cartAmount >= $minimumOrderValue) {
+            return;
+        }
+
+        $message = $this->translator->trans('StrackIntegrations.cart-checkout.cartBelowMinimumOrderValue', [
+            '%minOrderVal%' => $minimumOrderValue,
+        ]);
+
+        // Show flashbag only on these routes
+        $routesToShowFlashBag = [
+            "/checkout/offcanvas",
+            "/checkout/cart"
+        ];
+
+        $loadedRoute = $this->requestStack->getCurrentRequest()->get("resolved-uri");
+
+        if (!in_array($loadedRoute, $routesToShowFlashBag)) {
+            return;
+        }
+
+        $this->requestStack->getCurrentRequest()->getSession()->getFlashBag()->add('warning', $message);
+    }
+
+}
