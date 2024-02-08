392973ca35b07d55d52a6d2c80bdeff446233ec8
Christopher Falkner <c.falkner@agiqon.de>
2024-02-08 11:44:00 +0100

    build: move cart export feature to @StrackCartExport

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,6 +1,4 @@
 import PluginManager from 'src/plugin-system/plugin.manager';
 import QuantitySelectorCustomerPricePlugin from './quantity-selector-customer-price/quantity-selector-customer-price.plugin';
-import CartExporterPlugin from "./cart-exporter-plugin/cart-exporter-plugin";
 
 PluginManager.register('QuantitySelectorCustomerPrice', QuantitySelectorCustomerPricePlugin, '[data-quantity-selector-customer-price]');
-PluginManager.register('CartExporter', CartExporterPlugin, '[data-strack-cart-export-button]')
