1b071bca42b53e7861375b68149fd01bf6eff6da
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2024-02-09 11:48:47 +0100

    feat: added Accessories Page Subscriber to load live prices

---
+++ src/Subscriber/AccessoriesPageSubscriber.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StrackIntegrations\Subscriber;
+
+use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity;
+use StrackIntegrations\Client\PriceClient;
+use StrackIntegrations\Config\ApiConfig;
+use StrackIntegrations\Logger\Logger;
+use StrackIntegrations\Service\PriceTransformer;
+use StrackIntegrations\Struct\SalesPrice;
+use StrackOci\Models\OciSession;
+use StrackSwitchWizard\Page\AccessoriesPage\AccessoriesPageLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+readonly class AccessoriesPageSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private PriceClient $priceClient,
+        private PriceTransformer $priceTransformer,
+        private Logger $logger,
+        private ApiConfig $apiConfig
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            AccessoriesPageLoadedEvent::class => 'addCustomerPrices'
+        ];
+    }
+
+    public function addCustomerPrices(AccessoriesPageLoadedEvent $event): void
+    {
+        $customer = $event->getSalesChannelContext()->getCustomer();
+
+        if (!$customer) {
+            return;
+        }
+
+        $debtorNumber = $this->apiConfig->isTestModeOn() ? $this->apiConfig->getTestModeDebtorNumber() : $customer->getId();
+
+        if(!$this->apiConfig->isTestModeOn() && ($ociSession = $event->getRequest()->getSession()->get(OciSession::OCI_SESSION_NAME)) && $ociSession instanceof OciSession && $ociSession->getAdditionalFieldByKey('customer')) {
+            $debtorNumber = $ociSession->getAdditionalFieldByKey('customer')->getId();
+        }
+
+        $priceRequestBatch = [];
+
+        $page = $event->getPage();
+        $machineProducts = $page->getMachineSelectedProducts()->getAllSalesChannelProducts();
+
+        if($machineProducts->count() > 0) {
+            foreach($machineProducts as $product) {
+                $priceRequestBatch[$product->getProductNumber()] = $product->getMinPurchase() ?: 1;
+            }
+        }
+
+        $lightProducts = $event->getPage()->getLightSelectedProducts()->getAllSalesChannelProducts();
+        if($lightProducts->count() > 0) {
+            foreach($lightProducts as $product) {
+                $priceRequestBatch[$product->getProductNumber()] = $product->getMinPurchase() ?: 1;
+            }
+        }
+
+        if(count($priceRequestBatch) === 0) {
+            return;
+        }
+
+        try {
+            $customerPrices = $this->priceClient->getSalesPrices($debtorNumber, $priceRequestBatch, $event->getSalesChannelContext()->getCurrency()->getIsoCode());
+            if($machineProducts->count() > 0) {
+                foreach($page->getMachineSelectedProducts() as $selectedProduct) {
+                    if(!$selectedProduct->getProduct() instanceof SalesChannelProductEntity) {
+                        continue;
+                    }
+
+                    $customerPrice = $customerPrices->filter(function(SalesPrice $salesPrice) use($selectedProduct) {return $salesPrice->getProductNumber() === $selectedProduct->getProduct()->getProductNumber();})->first();
+                    if(!$customerPrice) {
+                        continue;
+                    }
+
+                    $this->priceTransformer->setCalculatedPrice($customerPrice, $selectedProduct->getProduct());
+                }
+            }
+            if($lightProducts->count() > 0) {
+                foreach($page->getLightSelectedProducts() as $selectedProduct) {
+                    if(!$selectedProduct->getProduct() instanceof SalesChannelProductEntity) {
+                        continue;
+                    }
+
+                    $customerPrice = $customerPrices->filter(function(SalesPrice $salesPrice) use($selectedProduct) {return $salesPrice->getProductNumber() === $selectedProduct->getProduct()->getProductNumber();})->first();
+                    if(!$customerPrice) {
+                        continue;
+                    }
+
+                    $this->priceTransformer->setCalculatedPrice($customerPrice, $selectedProduct->getProduct());
+                }
+            }
+        } catch(\Exception $exception) {
+            $this->logger->logException(self::class, $exception);
+            return;
+        }
+    }
+}
