939f66daf398c74a117a763c34ffb4663e0da2a4
Grzegorz Pietrzak <g.pietrzak@agiqon.de>
2024-03-15 10:11:52 +0100

    feat: added OrderController

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StrackIntegrations\Controller;
+
+use Shopware\Storefront\Controller\StorefrontController;
+
+class OrderController extends StorefrontController
+{
+
+}
